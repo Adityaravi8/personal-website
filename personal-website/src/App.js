3c538b597976e5c6373f129d77e77783ec5b28ec
Aditya Ravi <94879854+Adityaravi8@users.noreply.github.com>
2023-11-30 15:19:41 -0500

    Created Navbar and added new pages

--- personal-website/src/App.js
+++ personal-website/src/App.js
@@ -2,14 +2,16 @@ import "./App.css";
 import Navbar from "./components/Navbar.js";
 import About from "./components/About.js";
 import Projects from "./components/Projects.js";
+import Skills from "./components/Skills.js";
 import Contact from "./components/Contact.js";
 
 function App() {
   return (
-    <div>
+    <div className="bg-black w-full h-screen">
       <Navbar />
       <About />
       <Projects />
+      <Skills />
       <Contact />
     </div>
   );
--- personal-website/src/components/Navbar.js
+++ personal-website/src/components/Navbar.js
@@ -1,7 +1,45 @@
 import React from "react";
 
 function Navbar() {
-  return <div></div>;
+  return (
+    <div className="container flex flex-row items-center justify-between text-white font-serif">
+      <h1 className=" ml-5 text-2xl p-3">Aditya Ravi</h1>
+      <ul className="flex flex-row gap-10 text-2xl justify-end p-3">
+        <li>
+          <a
+            href="#About"
+            className="hover:underline underline-offset-8 decoration-4"
+          >
+            About
+          </a>
+        </li>
+        <li>
+          <a
+            href="#Projects"
+            className="hover:underline underline-offset-8 decoration-4"
+          >
+            Projects
+          </a>
+        </li>
+        <li>
+          <a
+            href="#Skills"
+            className="hover:underline underline-offset-8 decoration-4"
+          >
+            Skills
+          </a>
+        </li>
+        <li>
+          <a
+            href="#Contact"
+            className="hover:underline underline-offset-8 decoration-4"
+          >
+            Contact
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default Navbar;
