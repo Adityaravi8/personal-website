c9eb8e0b725f6a29290a348fc77d81e787465387
Aditya Ravi <94879854+Adityaravi8@users.noreply.github.com>
2024-09-14 12:19:09 -0400

    Setting Up Next Js and Tailwind

---
+++ personal-website/src/pages/index.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Aditya Ravi</title>
+      </Head>
+      <main></main>
+    </div>
+  );
+}
